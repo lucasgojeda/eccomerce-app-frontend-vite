ce31fa6e562ebc0d76bf102cbef3f911fc0d8612
lucasgojeda <lukitas.ojeda.00@gmail.com>
2022-12-04 23:21:34 -0300

    Work has begun on the functionality of search of products .

--- src/Application/search/pages/SearchPage.jsx
+++ src/Application/search/pages/SearchPage.jsx
@@ -1,32 +1,47 @@
 /** Libraries */
 import { styled } from "@mui/material/styles";
 
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
+
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+
 /** Components */
 import { CardProduct } from "../../home";
 import { Footer } from "../../ui";
 
 /** Custom hooks */
-import { useProductsStore } from "../../../hooks";
+import { useProductsStore, useUiStore } from "../../../hooks";
+import { Typography } from "@mui/material";
 
 /** Material UI - Custom components */
 const SearchPageContainer = styled("div")(({ theme }) => ({
   position: 'absolute',
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'space-between',
+  justifyContent: 'center',
   flexDirection: 'column',
   width: '100%',
-  minHeight: '100%',
+  minHeight: '100vh',
 }));
 
 const CardsContainer = styled("div")(({ theme }) => ({
-  width: '80%',
-  minHeight: '30vh',
+  width: '100%',
+  minHeight: '70vh',
   display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
   flexWrap: 'wrap',
+  gap: '2.5vw',
+  // alingContent: 'center',
+  // alignItems: 'center',
+  justifyContent: 'space-between',
   marginTop: '15vh',
+  [theme.breakpoints.between("600", "700")]: {
+    justifyContent: 'space-around',
+  },
+  [theme.breakpoints.down("sm")]: {
+    justifyContent: 'center',
+  },
 }));
 
 const SearchCardContainer = styled("div")(({ theme }) => ({
@@ -35,25 +50,111 @@ const SearchCardContainer = styled("div")(({ theme }) => ({
   marginRight: '2.5vh',
 }));
 
+const TitleFont = styled(Typography)(({ theme }) => ({
+  fontSize: '16px',
+  fontWeight: '700',
+  color: theme.palette.gray.main
+}));
+
+const mockProducts = [
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+  {
+    name: ''
+  },
+]
+
+
 export const SearchPage = () => {
   const { products } = useProductsStore();
+  const { searchProducts } = useUiStore();
+
+  const theme = useTheme();
+  const sm = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <SearchPageContainer>
-      {products.length === 0 || !products ? (
-        <CardsContainer>
-          <h3>No se ha encontrado ningún producto</h3>
-        </CardsContainer>
-      ) : (
-        <CardsContainer>
-          {products.map((e, i) => (
-            <SearchCardContainer key={i}>
-              <CardProduct product={e} />
-            </SearchCardContainer>
-          ))}
-        </CardsContainer>
-      )}
-      <Footer />
-    </SearchPageContainer>
+    <>
+      {
+        (!searchProducts)
+          ?
+          <SearchPageContainer>
+            {products.length === 0 || !products ? (
+              <CardsContainer>
+                <TitleFont variant="body2">
+                  No se ha encontrado ningún producto
+                </TitleFont>
+              </CardsContainer>
+            ) : (
+              <CardsContainer>
+                {products.map((e, i) => (
+                  <SearchCardContainer key={i}>
+                    <CardProduct product={e} />
+                  </SearchCardContainer>
+                ))}
+              </CardsContainer>
+            )}
+            <Footer />
+          </SearchPageContainer>
+          :
+          <SearchPageContainer>
+            <CardsContainer>
+              {
+                mockProducts.map((e, i) => <SearchCardContainer key={i}>
+                  <Stack spacing={1}>
+                    <Skeleton
+                      variant="rectangular"
+                      width={(sm) ? "17.5ch" : "20ch"}
+                      height="20ch"
+                      animation="wave"
+                    />
+                  </Stack>
+                </SearchCardContainer>
+                )
+              }
+            </CardsContainer>
+          </SearchPageContainer>
+      }
+    </>
+
   );
 };
