f7f9dee45bea8d943d96bafb0c6dd348fc86746e
lucasgojeda <lukitas.ojeda.00@gmail.com>
2022-12-04 23:14:43 -0300

    The store has been refactored and the code that it's unnecessary has been removed

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { authSlice } from './slices/authSlice';
-import { binSlice } from './slices/binSlice';
 import { cartSlice } from './slices/cartSlice';
 import { categoriesSlice } from './slices/categoriesSlice';
-import { staticsSlice } from './slices/staticsSlice';
 import { notificationsSlice } from './slices/notificationsSlice';
 import { productSlice } from './slices/productSlice';
-import { recordsSlice } from './slices/recordsSlice';
 import { saleSlice } from './slices/saleSlice';
 import { uiSlice } from './slices/uiSlice';
-import { userSlice } from './slices/userSlice';
 
 
 export const store = configureStore({
@@ -19,13 +15,9 @@ export const store = configureStore({
       auth: authSlice.reducer,
       product: productSlice.reducer,
       ui: uiSlice.reducer,
-      users: userSlice.reducer,
       sales: saleSlice.reducer,
-      records: recordsSlice.reducer,
       notifications: notificationsSlice.reducer,
-      statics: staticsSlice.reducer,
       cart: cartSlice.reducer,
       categories: categoriesSlice.reducer,
-      bin: binSlice.reducer,
   },
 })
