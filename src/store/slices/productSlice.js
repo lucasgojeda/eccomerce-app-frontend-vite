ec9c6d639a3f9e1f201165d525b29151bf451e49
lucasgojeda <lukitas.ojeda.00@gmail.com>
2022-12-04 23:09:13 -0300

    Product module has been refactored and the code that it's unnecessary has been removed

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -5,21 +5,8 @@ export const productSlice = createSlice({
   initialState: {
     products: [],
     bestProducts: [],
-    activeProduct: false,
   },
   reducers: {
-    setActiveProduct: (state, action) => {
-
-      state.activeProduct = action.payload;
-    },
-    clearActiveProduct: (state) => {
-
-      state.activeProduct = false;
-    },
-    clearFilteredProducts: (state) => {
-
-      state.filteredProducts = [];
-    },
     loadProducts: (state, action) => {
 
       state.products = [...action.payload];
@@ -28,48 +15,15 @@ export const productSlice = createSlice({
 
       state.bestProducts = [...action.payload];
     },
-    loadFilteredProducts: (state, action) => {
-
-      state.filteredProducts = action.payload;
-    },
-    addProduct: (state, action) => {
-
-      state.products = [
-        ...state.products,
-        action.payload
-      ];
-    },
-    updateProduct: (state, action) => {
-
-      state.products = state.products.map(
-        e => (e._id === action.payload._id) ? action.payload : e
-      );
-    },
-    deleteProduct: (state, action) => {
-
-      state.products = state.products.filter(
-        e => (e._id !== action.payload._id)
-      );
-      state.activeProduct = false;
-    },
     productsLogout: (state) => {
 
       state.products = [];
       state.bestProducts = [];
-      state.activeProduct = false;
     },
-
   },
 })
 
 export const {
-  setActiveProduct,
-  clearActiveProduct,
-  clearFilteredProducts,
   loadProducts,
-  loadFilteredProducts,
-  addProduct,
-  updateProduct,
-  deleteProduct,
   loadBestProducts,
   productsLogout, } = productSlice.actions;
